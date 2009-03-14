7a7e68b000c5d2123a4f7c773c7d62426d55c6af
anton.rau <anton.rau@2bbea778-08d1-11de-9c5c-7db7966a26d7>
2009-03-14 16:21:01 +0000

    ZXing integration prototype.
    git-svn-id: http://carbon-14.googlecode.com/svn/trunk@9 2bbea778-08d1-11de-9c5c-7db7966a26d7

--- Android/src/net/carbon14/MainActivity.java
+++ Android/src/net/carbon14/MainActivity.java
@@ -1,7 +1,11 @@
 package net.carbon14;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button buttonScan = (Button) findViewById(R.id.ButtonScan);
+        buttonScan.setOnClickListener(mScanListener);
     }
+    
+	// Create an anonymous implementation of OnClickListener
+    private OnClickListener mScanListener = new OnClickListener() {
+        public void onClick(View v) {
+        	// do something when the button is clicked
+        	Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            startActivityForResult(intent, 0);
+        }
+    };
 }
