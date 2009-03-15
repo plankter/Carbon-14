7d803ecc8898a191a5664a8a1593313386070f98
anton.rau <anton.rau@2bbea778-08d1-11de-9c5c-7db7966a26d7>
2009-03-15 18:53:12 +0000

    Initial development snapshot.
    git-svn-id: http://carbon-14.googlecode.com/svn/trunk@10 2bbea778-08d1-11de-9c5c-7db7966a26d7

--- Android/src/net/carbon14/MainActivity.java
+++ Android/src/net/carbon14/MainActivity.java
@@ -1,13 +1,22 @@
 package net.carbon14;
 
+import com.google.zxing.client.android.PreferencesActivity;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private final static int SCAN_REQUEST_CODE = 0;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,12 +27,59 @@ public class MainActivity extends Activity {
         buttonScan.setOnClickListener(mScanListener);
     }
     
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    
+    /* Handles item selections */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.scanMenuItem:
+        	StartScanning();
+            return true;
+        case R.id.profileMenuItem:
+            return true;
+        case R.id.settingsMenuItem:
+        	Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setClassName(this, PreferencesActivity.class.getName());
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+    
 	// Create an anonymous implementation of OnClickListener
     private OnClickListener mScanListener = new OnClickListener() {
         public void onClick(View v) {
-        	// do something when the button is clicked
-        	Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-            startActivityForResult(intent, 0);
+        	StartScanning();
         }
     };
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SCAN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                // Handle successful scan
+                ProductRegistered(contents, format);
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            	Toast.makeText(this, "Scanning Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    
+    private void ProductRegistered(String contents, String format)
+    {
+    	
+    }
+    
+    private void StartScanning()
+    {
+    	Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+    	intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+        startActivityForResult(intent, SCAN_REQUEST_CODE);
+    }
 }
