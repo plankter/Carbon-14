93dcce3a585bb3211b40eb82723fd2054109fc6c
anton.rau <anton.rau@2bbea778-08d1-11de-9c5c-7db7966a26d7>
2009-05-14 12:48:21 +0000

    git-svn-id: http://carbon-14.googlecode.com/svn/trunk@113 2bbea778-08d1-11de-9c5c-7db7966a26d7

--- Carbon-14 Android/src/net/carbon14/android/DecodeThread.java
+++ Carbon-14 Android/src/net/carbon14/android/DecodeThread.java
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * 
+ * Modified by Anton Rau.
+ * 
+ * 
  */
 
 package net.carbon14.android;
